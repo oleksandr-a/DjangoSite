86f8d9278f23e277bf7511b451bf742e94973a3e
oleksandr-a <o.albul@khai.edu>
2019-01-06 21:06:39 +0200

    Initial commit

---
+++ MFS1/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class Mfs1Config(AppConfig):
+    name = 'MFS1'
---
+++ MFS1/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+
+from .models import Question, Choice
+
+def index (request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'MFS1/index.html', context)
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'MFS1/detail.html', {'question': question})
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'MFS1/results.html', {'question': question})
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk.request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'MFS1/detail.html', {
+            'question': question,
+            'error_message': "You did not select a choice."
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('MFS1:results', args=(question_id,)))
